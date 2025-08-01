f349c7c19911d400ad7224f20faebefbfb0399bb
Le Dinh Tung <letung.apt@gmail.com>
2025-08-01 14:04:30 +0700

    hoàn thành trang web đơn giản

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
 import Header from '@/component/header';
-
-
+import Link from 'next/link';
 export default async function Home() {
+  const res = await fetch('http://localhost:3001/api/post/', {
+    cache: 'no-cache',
+  });
+  if(!res.ok) {
+    return(
+      <div>
+        <Header />
+        <div className="w-[800px] min-h-[70vh] p-4 text-[15px] mx-auto bg-[#d9eaff]">
+          <p>Không thể tải bài viết</p>
+        </div>
+      </div>
+    );
+  };
+  const posts = await res.json();
   return (
     <div>
       <Header />
       <div className="w-[800px] min-h-[70vh] p-4 text-[15px] mx-auto bg-[#d9eaff]">
-        
+        <h1 className='text-[20px] mb-4'>Tin tức mới nhất</h1>
+        <ul>
+          {
+            posts.data.map((post) => (
+              <li key={post.id} className='mb-4'>
+                <h3 className='hover:text-blue-600'><a href={`/post/${post.slug}`}><b>{post.title}</b></a></h3>
+                <p>{post.createdAt}</p>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     </div>
   );
--- src/component/header.js
+++ src/component/header.js
@@ -1,6 +1,26 @@
-export default function Header() {
+import Link from "next/link";
+export default async function Header() {
+    const res = await fetch('http://localhost:3001/api/category', {
+        cache: 'no-cache',
+    })
+    if(!res.ok) {
+        return(
+            <ul>
+                <li><Link href="/">Home</Link></li>
+            </ul>
+        );
+    };
+    const menu = await res.json();
     return (
-        <div className="w-[800px] h-[100px] mx-auto bg-[#efefef]">
-        </div>
+        <nav>
+            <ul className="flex align-center gap-4 w-[800px] min-h-[5vh] p-4 text-[15px] mx-auto bg-[#bebebe]">
+                <li className="hover:text-blue-600"><Link href="/">Home</Link></li>
+                {
+                    menu.data.map((item) => (
+                        <li key={item.id} className="hover:text-blue-600"><Link href={`/category/${item.slug}`}>{item.name}</Link></li>
+                    ))
+                }
+            </ul>
+        </nav>
     );
 }
